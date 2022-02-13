84306bc444eb859ca6a50ac845ad52203dc2d6f7
belain3d <belain3d@gmail.com>
2022-02-13 05:39:15 +0100

    Update No.3. Added new project: listFunctionsLib

---
+++ listFunctionsLib/example.py
@@ -0,0 +1,6 @@
+from functionsLib import *
+# A "functions" könyvtárból a következő függvények érhetők el:
+#  osszegzes(), megszamlalas(), eldontes(), kivalasztas(), maxKiv(), minKiv()
+
+t = [3, 8, 2, 4, 5, 1, 6]
+print(kivalasztas(t, 8)) # "2"-t fog kiprintelni, mivel a 8-as számjegy a második helyen van leghamarabb.
---
+++ listFunctionsLib/functionsLib.py
@@ -0,0 +1,61 @@
+# Összegzés függvény: összegzi egy adott lista/számsor számait | Sum of list elements.
+
+def osszegzes(lista):
+    osszeg = 0
+    for i in lista:
+        osszeg += i
+    return osszeg
+
+# Megszámlálás függvény: Megszámolja, hogy egy adott listában/sorban hány elem van | Counts the number of elements of the list.
+
+def megszamlalas(lista):
+    osszeg = 0
+    for i in lista:
+        osszeg += 1
+    return osszeg
+
+# Eldöntés függvény: Megmutatja, hogy a 2. argumentumként beütött érték benne van-e a listában/sorban | Shows if second argument
+# is in the list or not.
+
+def eldontes(lista, keresett):
+    n = len(lista)
+    i = 0
+
+    while i < n and lista[i] != keresett:
+        i += 1
+
+    if i < n:
+        return True
+    else:
+        return False
+
+# Kiválasztás függvény: Megnézi, hogy a 2. argumentumként beütött érték hanyadik az adott listában/sorban | Shows the second
+# argument's place in the list.
+
+def kivalasztas(lista, szam):
+    try:
+        i = 0
+        n = len(lista)
+        while lista[i] != szam:
+            i += 1
+        return i+1
+    except IndexError:
+        pass
+
+# Maximum kiválasztás függvény: kimutatja a legnagyobb értéket egy adott számsorban | Shows the largest number in the sequence.
+
+def maxKiv(lista):
+    maxElem = lista[0]
+    for elem in lista:
+        if elem > maxElem:
+            maxElem = elem
+    return maxElem
+
+# Minimum kiválasztás függvény: kimutatja a legkisebb értéket egy adott számsorban | Shows the smallest number in the sequence.
+
+def minKiv(lista):
+    minElem = lista[0]
+    for elem in lista:
+        if elem < minElem:
+            minElem = elem 
+    return minElem
